070d4db720e48a26768a493548ab16fb862c492e
kevinr99089 <93716347+kevinr99089@users.noreply.github.com>
2023-07-29 00:54:46 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -163,10 +163,9 @@ fi
 if youtube_t=$(toml_get_table "YouTube"); then youtube_mode=$(toml_get "$youtube_t" "build-mode") || youtube_mode="apk"; else youtube_mode="module"; fi
 if music_t=$(toml_get_table "Music"); then music_mode=$(toml_get "$music_t" "build-mode") || music_mode="apk"; else music_mode="module"; fi
 if [ "$youtube_mode" != module ] || [ "$music_mode" != module ]; then
-	log "\nInstall [Vanced Microg](https://github.com/TeamVanced/VancedMicroG/releases) for non-root YouTube or YT Music"
+	log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 fi
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
-log "\n---\nChangelog:"
-log "$(cat $TEMP_DIR/*-rv/changelog.md)"
+log "\nInstall [Vanced MicroG](https://github.com/inotia00/VancedMicroG/releases/latest) to be able to use non-root YouTube or Music"
+log "\nSee [builds for Extended](https://github.com/kevinr99089/Extended.Builder/releases/latest)."
 
 pr "Done"
