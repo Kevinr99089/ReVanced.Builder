06a1b3d1ded83a406466f64fe50eee134fbfde03
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2024-02-02 17:00:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -168,7 +168,7 @@ if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 log "\nInstall [MicroG](https://github.com/WSTxda/MicroG-RE/releases/latest) for use non-root YouTube and Music"
-log "\nSee [builds for Extended](https://github.com/kevinr99089/Extended.Builder/releases/latest)."
+log "\nSee [builds for AdVanced (formerly Extended)](https://github.com/kevinr99089/Extended.Builder/releases/latest)."
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
 if [ -n "$SKIPPED" ]; then
