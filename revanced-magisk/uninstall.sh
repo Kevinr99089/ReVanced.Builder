8ca41b553dd8fadb65fc99766b4e2557262f124d
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2024-12-06 14:15:59 +0100

    Add files via upload

--- revanced-magisk/uninstall.sh
+++ revanced-magisk/uninstall.sh
@@ -1,14 +1,9 @@
 #!/system/bin/sh
 {
 	MODDIR=${0%/*}
-<<<<<<< HEAD
 	. "$MODDIR/config"
 
 	rm "/data/adb/rvhc/${MODDIR##*/}.apk"
 	rmdir "/data/adb/rvhc"
 	rm "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
-=======
-	rm "$NVBASE/rvhc/${MODDIR##*/}".apk
-	rmdir "$NVBASE/rvhc"
->>>>>>> 71b9976 (Initial commit)
 } &
