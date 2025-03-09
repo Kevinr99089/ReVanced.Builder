718f1e8687e4abd8ba26f2dd55cc5d14f1e7db6c
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2025-03-09 19:51:55 +0100

    Test

--- build.sh
+++ build.sh
@@ -122,10 +122,14 @@ for table_name in $(toml_get_table_names); do
 		app_args[archive_dlurl]=${app_args[archive_dlurl]%/}
 		app_args[dl_from]=archive
 	} || app_args[archive_dlurl]=""
-	if [ -z "${app_args[dl_from]-}" ]; then abort "ERROR: no 'apkmirror_dlurl', 'uptodown_dlurl' or 'archive_dlurl' option was set for '$table_name'."; fi
+	if [ -z "${app_args[dl_from]-}" ]; then abort "ERROR: no 'apkmirror_dlurl', 'uptodown_dlurl', 'archive_dlurl' or 'filereport_dlurl' option was set for '$table_name'."; fi
 	app_args[arch]=$(toml_get "$t" arch) || app_args[arch]="all"
 	if [ "${app_args[arch]}" != "both" ] && [ "${app_args[arch]}" != "all" ] && [[ ${app_args[arch]} != "arm64-v8a"* ]] && [[ ${app_args[arch]} != "arm-v7a"* ]]; then
 		abort "wrong arch '${app_args[arch]}' for '$table_name'"
+	app_args[filereport_dlurl]=$(toml_get "$t" filereport-dlurl) && {
+		app_args[filereport_dlurl]=${app_args[filereport_dlurl]%/}
+		app_args[dl_from]=filereport
+	} || app_args[filereport_dlurl]=""
 	fi
 
 	app_args[include_stock]=$(toml_get "$t" include-stock) || app_args[include_stock]=true && vtf "${app_args[include_stock]}" "include-stock"
