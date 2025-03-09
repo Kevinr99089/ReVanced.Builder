73df6f8458d3201ea9339d16dec6004ddeb96d3e
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2025-03-09 20:22:07 +0100

    Revert changes

--- build.sh
+++ build.sh
@@ -122,28 +122,10 @@ for table_name in $(toml_get_table_names); do
 		app_args[archive_dlurl]=${app_args[archive_dlurl]%/}
 		app_args[dl_from]=archive
 	} || app_args[archive_dlurl]=""
-	if [ -z "${app_args[dl_from]-}" ]; then abort "ERROR: no 'apkmirror_dlurl', 'uptodown_dlurl', 'archive_dlurl' or 'filereport_dlurl' option was set for '$table_name'."; fi
+	if [ -z "${app_args[dl_from]-}" ]; then abort "ERROR: no 'apkmirror_dlurl', 'uptodown_dlurl' or 'archive_dlurl' option was set for '$table_name'."; fi
 	app_args[arch]=$(toml_get "$t" arch) || app_args[arch]="all"
 	if [ "${app_args[arch]}" != "both" ] && [ "${app_args[arch]}" != "all" ] && [[ ${app_args[arch]} != "arm64-v8a"* ]] && [[ ${app_args[arch]} != "arm-v7a"* ]]; then
 		abort "wrong arch '${app_args[arch]}' for '$table_name'"
-	app_args[filereport_dlurl]=$(toml_get "$t" filereport-dlurl) && {
-		app_args[filereport_dlurl]=${app_args[filereport_dlurl]%/}
-		app_args[dl_from]=filereport
-	} || app_args[filereport_dlurl]=""
- 	if [ "${app_args[dl_from]}" = "fileport" ]; then
-    echo "Downloading from FilePort: ${app_args[fileport_dlurl]}"
-    wget --content-disposition -O "Spotify.apk" "${app_args[fileport_dlurl]}" || \
-    curl -L -o "Spotify.apk" "${app_args[fileport_dlurl]}"
-    if [ $? -ne 0 ]; then
-        abort "ERROR: FilePort download failed."
-    fi
-
-    if [ -f "Spotify.apk" ]; then
-        echo "Download successful: Spotify.apk"
-        app_args[pkg_name]="com.spotify.music"
-    else
-        abort "ERROR: FilePort download failed, file not found."
-    fi
 	fi
 
 	app_args[include_stock]=$(toml_get "$t" include-stock) || app_args[include_stock]=true && vtf "${app_args[include_stock]}" "include-stock"
